a0ab839d5db3fc76a3b8b4ff583e97fc9f063533
tyrafero <lehanhunter@gmail.com>
2020-08-16 22:19:45 +0545

    Third Commit

--- app/src/main/java/com/example/ms3/Settings.java
+++ app/src/main/java/com/example/ms3/Settings.java
@@ -1,14 +1,46 @@
 package com.example.ms3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
 public class Settings extends AppCompatActivity {
+    private ChipNavigationBar ChipBar;
+    private Fragment fragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ChipBar=findViewById(R.id.chipBar);
+        ChipBar.setItemSelected(R.id.home,true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new homeFragment()).commit();
+
+        ChipBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                switch(i){
+                    case R.id.home:
+                        fragment=new homeFragment();
+                        break;
+                    case R.id.police:
+                        fragment=new locationFragment();
+                        break;
+                    case R.id.period:
+                        fragment=new periodFragment();
+                        break;
+                    case R.id.settings:
+                        fragment=new settingsFragment();
+                        break;
+
+                }
+                if(fragment!=null){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/ms3/settingsFragment.java
@@ -0,0 +1,105 @@
+package com.example.ms3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link settingsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class settingsFragment extends Fragment {
+
+    FirebaseAuth mAuth;
+    FirebaseFirestore mStore;
+    TextView pName,pEmail,pPhone;
+    String mName,mEmail,mPhone;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public settingsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment settingsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static settingsFragment newInstance(String param1, String param2) {
+        settingsFragment fragment = new settingsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+        //Yaha bata mero coding
+
+        mAuth = FirebaseAuth.getInstance();
+        mStore=FirebaseFirestore.getInstance();
+
+        DocumentReference docRef = mStore.collection("users").document(mAuth.getCurrentUser().getUid());
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+            if (documentSnapshot.exists()){
+                mName = documentSnapshot.getString("name");
+                mEmail=documentSnapshot.getString("email");
+                mPhone= mAuth.getCurrentUser().getPhoneNumber();
+
+                pName.setText(mName);
+                pEmail.setText(mEmail);
+                pPhone.setText(mPhone);
+            }
+            }
+        });
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_settings,container,false);
+        pName= view.findViewById(R.id.profileName);
+        pEmail =view.findViewById(R.id.profileEmail);
+        pPhone =view.findViewById(R.id.profilePhone);
+
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+}
