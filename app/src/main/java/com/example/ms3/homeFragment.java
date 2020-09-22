ce8de2d000734fec65b27710935ce2f0f56a7a06
tyrafero <lehanhunter@gmail.com>
2020-09-22 15:31:11 +0545

    Location Retrieved final

--- app/src/main/java/com/example/ms3/homeFragment.java
+++ app/src/main/java/com/example/ms3/homeFragment.java
@@ -3,6 +3,8 @@ package com.example.ms3;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 
@@ -12,6 +14,7 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,17 +26,24 @@ import com.firebase.geofire.GeoQuery;
 import com.firebase.geofire.GeoQueryEventListener;
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 
 public class homeFragment extends Fragment {
@@ -43,11 +53,12 @@ public class homeFragment extends Fragment {
     private LatLng userLocation;
     Location lastLocation;
     LocationRequest locationRequest;
+    private GoogleApiClient mLocationClient;
+    Location currentLocation;
+
 
     FirebaseDatabase rootNode;
     DatabaseReference reference;
-
-
 
 
     private FusedLocationProviderClient mFusedLocationClient;
@@ -61,23 +72,30 @@ public class homeFragment extends Fragment {
     }
 
 
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view= inflater.inflate(R.layout.fragment_home,container,false);
-        button1=view.findViewById(R.id.button5);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        Button bmain = view.findViewById(R.id.button);
+        button1 = view.findViewById(R.id.button5);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getActivity(),Button1.class);
+                Intent intent = new Intent(getActivity(), Button1.class);
                 startActivity(intent);
             }
         });
-
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this.getContext());
+        bmain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getCurrentLocation();
+            }
+        });
         e6 = view.findViewById(R.id.edittext);
         b6 = view.findViewById(R.id.b6);
+
+
         b6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -127,6 +145,44 @@ public class homeFragment extends Fragment {
         // Inflate the layout for this fragment
         return view;
     }
+
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                Location location = task.getResult();
+                if (location != null) {
+
+                    try {
+                        Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+                        List<Address>addresses= geocoder.getFromLocation(
+                                location.getLatitude(),location.getLongitude(),1
+                        );
+                        //String yaha cha
+                        //Yo check gara
+                        //Nilesh
+                        String rLocation= addresses.get(0).getAddressLine(0);
+                        Toast.makeText(getContext(),rLocation,Toast.LENGTH_LONG).show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+
+    }
+
 
 
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
