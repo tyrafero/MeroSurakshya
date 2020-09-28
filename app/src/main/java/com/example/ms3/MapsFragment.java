5a23907895f897c9d4ea85aac1f738fdfeda761d
tyrafero <lehanhunter@gmail.com>
2020-09-28 10:10:13 +0545

    test

--- app/src/main/java/com/example/ms3/MapsFragment.java
+++ app/src/main/java/com/example/ms3/MapsFragment.java
@@ -7,17 +7,20 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -99,6 +102,19 @@ GoogleApiClient.OnConnectionFailedListener,
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         b7=view.findViewById(R.id.button7);
+        ImageButton imgbut= view.findViewById(R.id.policecall);
+        imgbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:100"));
+                if(ActivityCompat.checkSelfPermission(getContext(),Manifest.permission.CALL_PHONE)!=
+                        PackageManager.PERMISSION_GRANTED){
+                    return;
+                }
+                startActivity(callIntent);
+            }
+        });
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this.getContext());
         b7.setOnClickListener(new View.OnClickListener() {
             @Override
